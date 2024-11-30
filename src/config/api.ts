36caf43a6324563c6fb15370132bdf311f73b59e
abnerpersio <binhopersio@gmail.com>
2024-11-30 13:11:59 -0300

    chore: api routes constants

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,14 @@
 export const API_BASE_PATH = process.env.EXPO_PUBLIC_API_URL;
+
+export const API_ROUTES = {
+  categories: {
+    list: '/categories',
+    products: '/categories/:category/products',
+  },
+  products: {
+    list: '/products',
+  },
+  orders: {
+    create: '/orders',
+  },
+};
